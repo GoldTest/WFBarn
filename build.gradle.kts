30bed77c9f0f720264cf38487c7824df8c6dc11d
Mage <1304747347@qq.com>
2025-12-25 20:16:27 +0800

    fix: explicitly set buildType=release in CI to trigger ProGuard optimization

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.wfbarn"
-version = "0.0.4"
+version = "0.0.5"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Exe, TargetFormat.Msi)
             packageName = "WFBarn"
-            packageVersion = "0.0.4"
+            packageVersion = "0.0.5"
             description = "WFBarn Money Management System"
             copyright = "© 2025 WFBarn"
             vendor = "WFBarn"
